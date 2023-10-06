c29e43b04d33c062bea162628fbe1b95cb3335f5
CodeSandbox <support@codesandbox.io>
2023-10-06 15:42:48 +0000

    Sandbox snapshot commit

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,93 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let draggingKey = "dragging";
+let dragged: HTMLElement | null = null;
+let display: string = "";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const observeDrop = (domNode: Node) => {
+  const targetNode = domNode;
+
+  const config = { childList: true, subtree: true };
+
+  const callback: MutationCallback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        if (dragged) {
+          const sel = document.getSelection();
+          if (
+            sel &&
+            sel.rangeCount &&
+            !!sel.getRangeAt(0).toString().match(draggingKey)
+          ) {
+            const range = sel.getRangeAt(0);
+            range.deleteContents();
+            range.insertNode(dragged);
+            range.collapse(true);
+            observer.disconnect();
+            mutation.target.normalize();
+          } else {
+            console.log({ mutation });
+            const node = mutation.addedNodes[0];
+            if (node?.textContent && node?.parentNode) {
+              const sel = document.getSelection();
+              sel?.removeAllRanges();
+              console.log("HERE!");
+              const start = node.textContent.indexOf(draggingKey);
+              const end = start + draggingKey.length;
+              const range = document.createRange();
+              range.setStart(node, start);
+              range.setEnd(node, end);
+              range.deleteContents();
+              range.insertNode(dragged);
+              range.collapse(true);
+              observer.disconnect();
+              mutation.target.normalize();
+            }
+          }
+        }
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+
+  return observer;
+};
+
+// const draggables = document.querySelectorAll(".draggable");
+const editor = document.querySelector('[contenteditable="true"');
+// startObserving(editor);
+
+if (editor) {
+  document.addEventListener("dragstart", (e: DragEvent) => {
+    const _draggable = e?.target as HTMLElement | null;
+    if (_draggable?.classList.contains("draggable") && e.dataTransfer) {
+      e.dataTransfer.clearData();
+      e.dataTransfer.setData("text/plain", draggingKey);
+      dragged = _draggable;
+      observeDrop(editor);
+      setTimeout(() => {
+        if (dragged) {
+          display = dragged.style.display;
+          dragged.style.display = "none";
+        }
+      });
+    }
+  });
+
+  editor.addEventListener("dragend", (e) => {
+    const _draggable = e?.target as HTMLElement | null;
+    if (_draggable?.classList.contains("draggable")) {
+      dragged = _draggable;
+      setTimeout(() => {
+        if (dragged) {
+          dragged.style.display = display;
+        }
+      });
+    }
+  });
+}
